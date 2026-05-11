d06e824d3ceeb295bb409182158ee3f964c8597e
gerios777 <georgetarabain2017@gmail.com>
2026-05-11 21:53:16 +0300

    add drating to book details

--- lib/features/Home/Presentation/Views/widgets/book_details_view_body.dart
+++ lib/features/Home/Presentation/Views/widgets/book_details_view_body.dart
@@ -1,4 +1,5 @@
 import 'package:bookly_app/core/utils/styles.dart';
+import 'package:bookly_app/features/Home/Presentation/Views/widgets/book_rating.dart';
 import 'package:bookly_app/features/Home/Presentation/Views/widgets/custom_book_details_app_bar.dart';
 import 'package:bookly_app/features/Home/Presentation/Views/widgets/custom_book_image.dart';
 import 'package:flutter/material.dart';
@@ -33,6 +34,8 @@ class BookDetailsViewBody extends StatelessWidget {
             ),
           ),
         ),
+        const SizedBox(height: 18),
+        const BookRating(mainAxisAlignment: MainAxisAlignment.center),
       ],
     );
   }
--- lib/features/Home/Presentation/Views/widgets/book_rating.dart
+++ lib/features/Home/Presentation/Views/widgets/book_rating.dart
@@ -3,24 +3,28 @@ import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class BookRating extends StatelessWidget {
-  const BookRating({super.key});
-
+  const BookRating({
+    super.key,
+    this.mainAxisAlignment = MainAxisAlignment.start,
+  });
+  final MainAxisAlignment mainAxisAlignment;
   @override
   Widget build(BuildContext context) {
     return Row(
+      mainAxisAlignment: mainAxisAlignment,
       children: [
         const FaIcon(
           FontAwesomeIcons.solidStar,
           color: Color(0xffFFDD4F),
-          size: 16,
+          size: 14,
         ),
         const SizedBox(width: 6.3),
         const Text("4.8", style: Styles.textStyle16),
         const SizedBox(width: 5),
 
-        Text(
-          "(2390)",
-          style: Styles.textStyle14.copyWith(color: Colors.white54),
+        const Opacity(
+          opacity: .5,
+          child: Text("(2390)", style: Styles.textStyle14),
         ),
       ],
     );
