c5da8d9243c1bfc5bf423442bae909dc6217a2d1
gerios777 <georgetarabain2017@gmail.com>
2026-05-12 20:13:03 +0300

    create books button actions

---
+++ lib/core/widgets/custom_button.dart
@@ -0,0 +1,41 @@
+import 'package:bookly_app/core/utils/styles.dart';
+import 'package:flutter/material.dart';
+
+class CustomButton extends StatelessWidget {
+  const CustomButton({
+    super.key,
+    required this.backgroundColor,
+    required this.textColor,
+    this.borderRadius,
+    required this.text,
+    this.fontSize,
+  });
+  final double? fontSize;
+  final String text;
+  final Color backgroundColor;
+  final Color textColor;
+  final BorderRadius? borderRadius;
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 48,
+      child: TextButton(
+        style: TextButton.styleFrom(
+          shape: RoundedRectangleBorder(
+            borderRadius: borderRadius ?? BorderRadius.circular(12),
+          ),
+          backgroundColor: backgroundColor,
+        ),
+        child: Text(
+          text,
+          style: Styles.texstStyle18.copyWith(
+            color: textColor,
+            fontWeight: FontWeight.w900,
+            fontSize: fontSize ?? 18,
+          ),
+        ),
+        onPressed: () {},
+      ),
+    );
+  }
+}
--- lib/features/Home/Presentation/Views/widgets/book_details_view_body.dart
+++ lib/features/Home/Presentation/Views/widgets/book_details_view_body.dart
@@ -1,5 +1,6 @@
 import 'package:bookly_app/core/utils/styles.dart';
 import 'package:bookly_app/features/Home/Presentation/Views/widgets/book_rating.dart';
+import 'package:bookly_app/features/Home/Presentation/Views/widgets/books_action.dart';
 import 'package:bookly_app/features/Home/Presentation/Views/widgets/custom_book_details_app_bar.dart';
 import 'package:bookly_app/features/Home/Presentation/Views/widgets/custom_book_image.dart';
 import 'package:flutter/material.dart';
@@ -36,6 +37,8 @@ class BookDetailsViewBody extends StatelessWidget {
         ),
         const SizedBox(height: 18),
         const BookRating(mainAxisAlignment: MainAxisAlignment.center),
+        const SizedBox(height: 37),
+        const Padding(padding: EdgeInsets.all(8.0), child: BooksAction()),
       ],
     );
   }
