73a18a9b1bc5eeee6c3b6ccf90b727fa0421ef33
gerios777 <georgetarabain2017@gmail.com>
2026-05-17 19:12:05 +0300

    create fetured books method in the cubit

--- lib/features/Home/Presentation/view_models/fetured_books_cubit/fetured_books_cubit.dart
+++ lib/features/Home/Presentation/view_models/fetured_books_cubit/fetured_books_cubit.dart
@@ -1,9 +1,23 @@
 import 'package:bloc/bloc.dart';
 import 'package:bookly_app/features/Home/Data/models/book_model/book_model.dart';
+import 'package:bookly_app/features/Home/Data/repos/home_repo.dart';
 import 'package:equatable/equatable.dart';
 
 part 'fetured_books_cubit_state.dart';
 
 class FeturedBooksCubit extends Cubit<FeturedBooksCubitState> {
-  FeturedBooksCubit() : super(FeturedBooksCubitInitial());
+  FeturedBooksCubit(this.homeRepo) : super(FeturedBooksCubitInitial());
+
+  final HomeRepo homeRepo;
+
+  Future<void> FetchFeaturedBooks() async {
+
+    emit(FeturedBooksCubitLoading());
+    var result = await homeRepo.fetchFeaturedBooks();
+    result.fold((failure) {
+      emit(FeturedBooksFailure(failure.errorMassage));
+    }, (books) {
+      emit(FeturedBooksCubitSuccess(books));
+    });
+  }
 }
